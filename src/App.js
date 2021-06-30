4e5455a925dbc50d355102cd27b661f08ecc2e41
Udip Parajuli <udipparajuli17@gmail.com>
2021-06-30 15:29:39 +0545

    Added the route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavouritePage from './pages/Favourite';
+
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Switch>
+      <Route path='/' exact={true}>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favourites'>
+        <FavouritePage />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+function NewMeetupPage() {
+    return <div>New MeetUps Page</div>
+}
+
+export default NewMeetupPage;
